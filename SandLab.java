ab9e37498eedb1eeb5f2c372afa5f38f19d4970e
Eason Lu <ghcodesign@outlook.com>
2023-10-15 16:18:40 -0700

    POP stash

--- SandLab.java
+++ SandLab.java
@@ -78,49 +78,12 @@ public class SandLab
     Random rand = new Random();
     int row = rand.nextInt(grid.length);
     int col = rand.nextInt(grid[0].length);
+    //add sand and water interaction, sand will always displace water, so if they touch sand and water will trade places, and will always do down and water will go up
     if (grid[row][col] == SAND)
     {
-      if (row + 1 < grid.length && grid[row + 1][col] == EMPTY)
+      if(grid[row + 1][col] == EMPTY && row + 1 < grid.length)
       {
         grid[row + 1][col] = SAND;
-        grid[row][col] = EMPTY;
-      }
-      else if (row + 1 < grid.length && col + 1 < grid[0].length && grid[row + 1][col + 1] == EMPTY)
-      {
-        grid[row + 1][col + 1] = SAND;
-        grid[row][col] = EMPTY;
-      }
-      else if (row + 1 < grid.length && col - 1 >= 0 && grid[row + 1][col - 1] == EMPTY)
-      {
-        grid[row + 1][col - 1] = SAND;
-        grid[row][col] = EMPTY;
-      }
-    }
-    else if (grid[row][col] == WATER)
-    {
-      if (row + 1 < grid.length && grid[row + 1][col] == EMPTY)
-      {
-        grid[row + 1][col] = WATER;
-        grid[row][col] = EMPTY;
-      }
-      else if (row + 1 < grid.length && col + 1 < grid[0].length && grid[row + 1][col + 1] == EMPTY)
-      {
-        grid[row + 1][col + 1] = WATER;
-        grid[row][col] = EMPTY;
-      }
-      else if (row + 1 < grid.length && col - 1 >= 0 && grid[row + 1][col - 1] == EMPTY)
-      {
-        grid[row + 1][col - 1] = WATER;
-        grid[row][col] = EMPTY;
-      }
-      else if (col + 1 < grid[0].length && grid[row][col + 1] == EMPTY)
-      {
-        grid[row][col + 1] = WATER;
-        grid[row][col] = EMPTY;
-      }
-      else if (col - 1 >= 0 && grid[row][col - 1] == EMPTY)
-      {
-        grid[row][col - 1] = WATER;
         grid[row][col] = EMPTY;
       }
     }
