21dfc656714a32ba8910939b07291a427292dea5
Eason Lu <ghcodesign@outlook.com>
2023-10-15 15:41:59 -0700

    Prevent overwride of particles

--- SandLab.java
+++ SandLab.java
@@ -8,7 +8,7 @@ public class SandLab
     SandLab lab = new SandLab(120, 80);
     lab.run();
   }
-  
+  //Colors
   //add constants for particle types here
   public static final int EMPTY = 0;
   public static final int METAL = 1;
@@ -16,7 +16,7 @@ public class SandLab
   public static final int WATER = 3;
   
   //do not add any more fields
-  private int[][] grid;
+  private int[][] grid = new int[120][80];
   private SandDisplay display;
   
   public SandLab(int numRows, int numCols)
@@ -33,11 +33,42 @@ public class SandLab
   //called when the user clicks on a location using the given tool
   private void locationClicked(int row, int col, int tool)
   {
+    //prevent override existing particles by other particles
+    if (grid[row][col] == EMPTY)
+    {
+      grid [row][col] = tool;
+    }else if(tool == EMPTY)
+    {
+      grid [row][col] = tool;
+    }
   }
 
   //copies each element of grid into the display
   public void updateDisplay()
   {
+    final Color LIGHT_BLUE = new Color(51,153,255);
+    for (int row = 0; row < grid.length; row++)
+    {
+      for (int col = 0; col < grid[0].length; col++)
+      {
+        if (grid[row][col] == EMPTY)
+        {
+          display.setColor(row, col, Color.BLACK);
+        }
+        else if (grid[row][col] == METAL)
+        {
+          display.setColor(row, col, Color.GRAY);
+        }
+        else if (grid[row][col] == SAND)
+        {
+          display.setColor(row, col, Color.YELLOW);
+        }
+        else if (grid[row][col] == WATER)
+        {
+          display.setColor(row, col, LIGHT_BLUE);
+        }
+      }
+    }
   }
 
   //called repeatedly.
